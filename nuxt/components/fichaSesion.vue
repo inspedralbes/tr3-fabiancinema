0eaa9cebdc8ed955806771352bc816c6f999f31b
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-04-09 08:54:26 +0200

    fix error

--- nuxt/components/fichaSesion.vue
+++ nuxt/components/fichaSesion.vue
@@ -1,24 +1,28 @@
 <template>
-    <div v-if="sesion">
-        <p class="info-sesion">{{ sesion.dia }}</p>
-        <p class="info-sesion">{{ sesion.hora }}</p>
-    </div>
+  <div v-if="sesion">
+    <p class="session-info">{{ sesion.dia }}</p>
+    <p class="session-info">{{ sesion.hora }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        sesion: {
-            type: Object,
-            required: true,
-        },
-        pelicula: {
-            type: Object,
-            required: true,
-        },
+  props: {
+    sesion: {
+      type: Object,
+      required: true,
     },
+    pelicula: {
+      type: Object,
+      required: true,
+    },
+  },
 }
 </script>
 
-<style>
+<style scoped>
+.session-info {
+  margin: 0;
+  font-size: 0.9rem;
+}
 </style>
